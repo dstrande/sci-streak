693af40b1bf4dd077b54c16064d36af8c513896f
Dallas Strandell <41165864+dstrande@users.noreply.github.com>
2022-08-29 13:43:25 -0400

    Updated streakgui.py and added stylesheet.qss

--- gui/streakgui.py
+++ gui/streakgui.py
@@ -1,4 +1,4 @@
-from PySide6 import QtCore, QtWidgets, QtGui, QtQuick
+from PySide6 import QtCore, QtWidgets, QtGui
 
 import pyqtgraph as pg
 import numpy as np
@@ -7,95 +7,56 @@ import ctypes
 import json
 import glob
 import h5py
+from scipy import interpolate
+from scipy.signal import decimate
 from functools import partial
+import time as timing
 
 
 myappid = 'sci.streak'  # arbitrary string
 ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
-
-# data = np.loadtxt('ds186_00_cwl500_700ps.txt', skiprows=16)
-# wavel = data[0, 1:]
-# time = data[1:, 0]
-# inten = data[1:, 1:]
 
 with open('data/experiment.json', 'r', encoding='utf-8') as f:
     experiment = json.load(f)
     experiment_list = list(experiment.keys())
     experiment_list.insert(0, '#')
 
-bar_colors = """
-QMainWindow
-{
-    background:rgba(20,20,20,255);
-    color:#fcffa4;
-    font-weight:bold;
-}
-QTreeWidget
-{
-    background:rgba(30,30,30,255);
-    color:#fcffa4;
-}
-QTreeWidget QHeaderView
-{
-    background:rgba(30,30,30,255);
-    background-color:rgba(30,30,30,255);
-    color:#fcffa4;
-}
-QTreeWidget QHeaderView:section
-{
-    background:rgba(30,30,30,255);
-    background-color:rgba(30,30,30,255);
-    color:#fcffa4;
-}
-QSplitter::handle
-{
-    background:rgba(50,50,50,255);
-}
-QStatusBar
-{
-    background:rgba(20,20,20,255);
-    color:#fcffa4;
-    font-weight:bold;
-}
-QMenuBar
-{
-    background:rgba(30,30,30,255);
-    color:#fcffa4;
-}
-QMenuBar::item
-{
-    background:rgba(50,50,50,255);
-    color:#fcffa4;
-}
-QMenuBar::item::selected
-{
-    background:rgba(132,32,107,150);
-    color:#fcffa4;
-    font-weight:bold;
-}
-QMenu
-{
-    background:rgba(132,32,107,100);
-    color:#fcffa4;
-    font-weight:bold;
-}
-QMenu::item::selected
-{
-    background:rgba(132,32,107,255);
-    color:#fcffa4;
-    font-weight:bold;
-}
-"""
+with open('gui/ui/stylesheet.qss', 'r', encoding='utf-8') as file:
+    stylesheet = file.read()
 
 
 class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-        self.setStyleSheet(bar_colors)
+        self.setStyleSheet(stylesheet)
         self.initUI()
 
     def initUI(self):
+        self.menuBarOutline()
+        self.statusBar().showMessage('Ready')
+        self.statusBar().setStyleSheet(stylesheet)
+
+        self.setWindowIcon(QtGui.QIcon('gui/icons/icon.png'))
+        self.setWindowTitle('sci-streak')
+        self.resize(800, 600)  # Fix for not starting maximized.
+        self.showMaximized()
+
+        # Window Layout
+        self.mainWidget = QtWidgets.QWidget()
+        self.hbox = QtWidgets.QHBoxLayout()
+        self.splitter1 = QtWidgets.QSplitter(QtCore.Qt.Horizontal)
+        self.splitter1.setStyleSheet(stylesheet)
+        self.setCentralWidget(self.mainWidget)
+
+        # Populate Layout
+        self.plotLayout()
+        self.splitter1.addWidget(self.widget)
+        self.treeWidget()
+        self.hbox.addWidget(self.splitter1)
+        self.mainWidget.setLayout(self.hbox)
+
+    def menuBarOutline(self):
         exitAction = QtGui.QAction(QtGui.QIcon('gui/icons/exit.png'), '&Exit', self)
         exitAction.setShortcut('Ctrl+Q')
         exitAction.setStatusTip('Exit application')
@@ -105,26 +66,25 @@ class MainWindow(QtWidgets.QMainWindow):
         self.fileMenu.addAction(exitAction)
         self.analyzeMenu = self.menuBar().addMenu('&Analysis')
         self.settingsMenu = self.menuBar().addMenu('&Settings')
-        self.menuBar().setStyleSheet(bar_colors)
+        self.colorMenu = self.settingsMenu.addMenu('Choose Colormap')
 
-        self.statusBar().showMessage('Ready')
-        self.statusBar().setStyleSheet(bar_colors)
+        self.inferno = QtGui.QAction('inferno', self)
+        self.colorMenu.addAction(self.inferno)
+        self.inferno.triggered.connect(partial(self.changeColormap, 'inferno'))
+        self.turbo = QtGui.QAction('turbo', self)
+        self.colorMenu.addAction(self.turbo)
+        self.turbo.triggered.connect(partial(self.changeColormap, 'turbo'))
+        self.viridis = QtGui.QAction('viridis', self)
+        self.colorMenu.addAction(self.viridis)
+        self.viridis.triggered.connect(partial(self.changeColormap, 'viridis'))
+        self.spectral = QtGui.QAction('spectral', self)
+        self.colorMenu.addAction(self.spectral)
+        self.spectral.triggered.connect(partial(self.changeColormap, 'nipy_spectral'))
 
-        self.setWindowIcon(QtGui.QIcon('gui/icons/icon.png'))
-        self.setWindowTitle('sci-streak')
-        self.resize(800, 600)
-        self.showMaximized()
+        self.menuBar().setStyleSheet(stylesheet)
 
-        # Window Layout
-        self.mainWidget = QtWidgets.QWidget()
-        self.hbox = QtWidgets.QHBoxLayout()
-        self.splitter1 = QtWidgets.QSplitter(QtCore.Qt.Horizontal)
-        self.splitter1.setStyleSheet(bar_colors)
-        self.setCentralWidget(self.mainWidget)
-
-        # The plotting
+    def plotLayout(self):
         self.widget = pg.GraphicsLayoutWidget()
-
         wavel, time, self.inten = self.openhdf5(0)
         self.plot(wavel, time, self.inten)
         self.hist()
@@ -143,25 +103,18 @@ class MainWindow(QtWidgets.QMainWindow):
         self.updateDecayROI()
         self.updateSpectrumROI()
 
-        self.splitter1.addWidget(self.widget)
-        self.treeWidget()
-        self.hbox.addWidget(self.splitter1)
-        self.mainWidget.setLayout(self.hbox)
-
     def plot(self, x, y, z):
         self.ax2D = self.widget.addPlot(row=0, col=0, colspan=5)
         self.img = pg.ImageItem()
-        print(z.shape)
+        # print(z.shape)
         self.img.setImage(z)
         self.ax2D.addItem(self.img)
-        self.ax2D.showAxes(True)
 
         # Move the image by half a pixel so that the center of the pixels are
         # located at the coordinate values
         dx = x[1] - x[0]
         dy = y[1] - y[0]
-        print("pixel size x: {}, pixel size y: {}".format(dx, dy))
-
+        print('pixel size x: {}, pixel size y: {}'.format(dx, dy))
         rect = QtCore.QRectF(x[0] - dx / 2, y[0] - dy / 2, x[-1] - x[0], y[-1] - y[0])
         print(rect)
         self.img.setRect(rect)
@@ -171,14 +124,12 @@ class MainWindow(QtWidgets.QMainWindow):
     def updatePlot(self, x, y, z):
         self.img.setImage(z)
 
-        print(z.shape)
+        # print(z.shape)
         # Move the image by half a pixel so that the center of the pixels are
         # located at the coordinate values
         dx = x[1] - x[0]
         dy = y[1] - y[0]
-        print("pixel size x: {}, pixel size y: {}".format(dx, dy))
-        print(x[0])
-        print(x[-1])
+        print('pixel size x: {}, pixel size y: {}'.format(dx, dy))
         rect = QtCore.QRectF(x[0] - dx / 2, y[0] - dy / 2, x[-1] - x[0], y[-1] - y[0])
         print(rect)
         self.img.setRect(rect)
@@ -189,7 +140,9 @@ class MainWindow(QtWidgets.QMainWindow):
         maxi = np.max(self.inten) / 2
         mini = np.average(self.inten) + 0.2
         self.histItem.setImageItem(self.img)
-        self.histItem.gradient.loadPreset('inferno')
+        cmap = pg.colormap.get('inferno', source='matplotlib', skipCache=False)
+        self.histItem.gradient.setColorMap(cmap)
+        self.histItem.gradient.showTicks(show=False)
         self.histItem.setLevels(mini, maxi)
         self.widget.addItem(self.histItem, row=0, col=5, colspan=1)
 
@@ -209,19 +162,13 @@ class MainWindow(QtWidgets.QMainWindow):
     def updateDecayROI(self):
         selected = self.roi.getArrayRegion(self.inten, self.img, returnMappedCoords=True)
         axis_select = 1
-        if axis_select == 0:
-            xaxis = selected[1][1][1]
-        else:
-            xaxis = selected[1][0][:, 0]
+        xaxis = selected[1][0][:, 0]
         self.decay_plot.plot(xaxis, selected[0].mean(axis=axis_select), clear=True)
 
     def updateSpectrumROI(self):
         selected = self.roi.getArrayRegion(self.inten, self.img, returnMappedCoords=True)
         axis_select = 0
-        if axis_select == 0:
-            xaxis = selected[1][1][1]
-        else:
-            xaxis = selected[1][0][:, 0]
+        xaxis = selected[1][1][0]
         self.spectrum_plot.plot(xaxis, selected[0].mean(axis=axis_select), clear=True)
 
     def treeWidget(self):
@@ -243,23 +190,68 @@ class MainWindow(QtWidgets.QMainWindow):
                 self.treeParents[i].setText(x, str(experiment[experiment_list[x]][i]))
                 # self.treeParents[i].setWidget(x, QtWidgets.QLabel(str(experiment[experiment_list[x]][i])))
 
-        self.log_widget.setStyleSheet(bar_colors)
+        self.log_widget.setStyleSheet(stylesheet)
 
     def openhdf5(self, idx):
-        with h5py.File(glob.glob("data/*.hdf5")[0], 'r') as f:
+        """
+        Method to import data for an .hdf5 file.
+        Note that there should be only one hdf5 file in the working directory.
+        """
+        with h5py.File(glob.glob('data/*.hdf5')[0], 'r') as f:
             if isinstance(idx, str):
                 pass
             else:
                 idx = str(idx)
             data = np.array(f.get(str(idx)))
-            wavel = data[1:, 0]
-            time = data[0, 1:]
-            inten = data[1:, 1:]
 
-            return wavel, time, inten
+        wavel = data[1:, 0]
+        time = data[0, 1:]
+        inten = data[1:, 1:]
+
+        xlabels, ylabels, data = self.downsample(wavel, time, inten)
+
+        return xlabels, ylabels, data
+
+    def downsample_factor(self):
+        """
+        TODO: add slider and button to choose downsample size.
+        Currently fixed to 2.
+        See the downsample method.
+        """
+        return 2
+
+    def downsample(self, wavel, time, inten):
+        """
+        Method used to downsample or rebin data for speed reasons.
+        There is a trade off between speed and resolution.
+        Note that the energy and time resolution is much less than the pixel size.
+        e.g. time resolution of ~4 ps and pixel size of ~0.35ps per pixel.
+        """
+        # Currently using the mean window sizes for the final step size.
+        step_x = np.ediff1d(wavel).mean()
+        step_y = np.ediff1d(time).mean()
+
+        # Function to generate interpolated values from our irregular grid
+        t0 = timing.time()
+        f = interpolate.RectBivariateSpline(wavel, time, inten)
+        t1 = timing.time()
+        print(f'Interpolate time: {t1 - t0}')
+
+        # Generate new data on the regular grid
+        xlabels = np.arange(wavel[0], wavel[-1] + step_x, step_x)
+        ylabels = np.arange(time[0], time[-1] + step_y, step_y)
+        data = f(xlabels, ylabels)
+
+        print(data.shape)
+        data = decimate(data, self.downsample_factor, axis=0)
+        data = decimate(data, self.downsample_factor, axis=1)
+        xlabels = decimate(xlabels, self.downsample_factor)
+        ylabels = decimate(ylabels, self.downsample_factor)
+        print(data.shape)
+
+        return xlabels, ylabels, data
 
     def button(self, idx):
-        print(idx)
         if type(idx) == int:
             wavel, time, self.inten = self.openhdf5(idx)
             self.updatePlot(wavel, time, self.inten)
@@ -268,6 +260,11 @@ class MainWindow(QtWidgets.QMainWindow):
             self.histItem.setLevels(mini, maxi)
             self.updateSpectrumROI()
             self.updateDecayROI()
+
+    def changeColormap(self, cmapstr):
+        cmap = pg.colormap.get(cmapstr, source='matplotlib', skipCache=False)
+        self.histItem.gradient.setColorMap(cmap)
+        self.histItem.gradient.showTicks(show=False)
 
 
 def application():
